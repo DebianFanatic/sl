3b9295256e6434719efb0aa48e40e2c5de6d1513
Kent West <kent.west@acu.edu>
2023-08-01 13:32:49 -0500

    Added fly option.

--- src/get_options.rs
+++ src/get_options.rs
@@ -1,8 +1,8 @@
 /* get_options.rs */
 
-pub fn get_opts() -> String {
-    use clap::Parser; // Use c[ommand] l[ine] a[rgument] p[arser] to get command-line arguments.
+pub fn get_opts() -> (String, bool) {
     use crate::images::*;
+    use clap::Parser; // Use c[ommand] l[ine] a[rgument] p[arser] to get command-line arguments.
 
     // Set up program-argument possibilities.
     #[derive(Parser)]
@@ -10,21 +10,33 @@ pub fn get_opts() -> String {
         /// Which model should be used?
         #[arg(long,short, default_value_t = String::from("D51"))]
         model: String,
+
+        /// Should the model fly?
+        #[arg(long, short, default_value_t = false)]
+        fly: bool,
     } // end of Arguments struct definition
 
     let args: Arguments = Arguments::parse();
 
     let image_str = match args.model.to_uppercase().as_str() {
-      "P" | "PLANE" => PLANE,
-      "D" | "D51" => D51,
-      "C" | "C51" => C51,
-      "L" | "LITTLE" => LITTLE,
-      "B" | "BOAT" => BOAT,
-      "T" | "TWIN" | "TWINENGINE" => TWINENGINE,
-      "M" | "MOTOR" | "MOTORCYCLE" | "CYCLE" => MOTORCYCLE,
-      "J" | "JACK" => JACK,
-      "F" | "FERRIS" | "MASCOT" => FERRIS,
-      _ => D51,
+        "P" | "PLANE" => PLANE,
+        "D" | "D51" => D51,
+        "C" | "C51" => C51,
+        "L" | "LITTLE" => LITTLE,
+        "B" | "BOAT" => BOAT,
+        "T" | "TWIN" | "TWINENGINE" => TWINENGINE,
+        "M" | "MOTOR" | "MOTORCYCLE" | "CYCLE" => MOTORCYCLE,
+        "J" | "JACK" => JACK,
+        "F" | "FERRIS" | "MASCOT" => FERRIS,
+        _ => D51,
     };
-    return image_str.to_string();
+
+    let mut fly: bool = false;
+    if args.fly {
+        fly = true;
+    } else {
+        fly = false;
+    }
+
+    return (image_str.to_string(), fly);
 } // end of get_options()
--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,8 @@ use display::display_image;
 use get_options::get_opts;
 
 fn main() {
-    let model_string = get_opts();
+    let (model_string, fly) = get_opts();
     let model_vecvecstring = str_to_vecvecstring(&model_string);
     display_image(model_vecvecstring);
+    println!("The option to fly = {}.", fly); // Temporary test of what we get back in "fly".
 } // end of main()
