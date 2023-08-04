028b84e72d8ec6b755f60e46c9652c2cbed1b9e9
Kent West <kent.west@acu.edu>
2023-08-04 16:24:40 -0500

    ncurses time to test first changes

--- src/main.rs
+++ src/main.rs
@@ -8,12 +8,21 @@ mod images;
 use convert_to_vec::str_to_vecvecstring;
 use display::display_image;
 use get_options::get_opts;
+use ncurses::*;
 
 fn main() {
     let (model_string, fly, oops, interruptible) = get_opts();
     let model_vecvecstring = str_to_vecvecstring(&model_string);
+
+    // ncurses housekeeping stuff.
+    initscr(); // Start ncurses, initializing the screen.
+    noecho(); // Turn off keyboard echo to the screen.
+    curs_set(CURSOR_VISIBILITY::CURSOR_INVISIBLE); // Turn off the display of the cursor.
+    let mut screen_height = 0; // How high...
+    let mut screen_width = 0; // ... and wide ...
+    getmaxyx(stdscr(), &mut screen_height, &mut screen_width); // ... is the screen?
+
     display_image(model_vecvecstring);
-    println!("The option to fly = {}.", fly); // Temporary test of what we get back in "fly".
-    println!("The Accident (\"oops\") option = {}.", oops); // Temporary test of what we get back in "oops";
-    println!("The \"interruptible\") option = {}.", interruptible); // Temporary test of what we get back in "interruptible";
+
+    endwin(); // Exit ncurses and restore screen to normal.
 } // end of main()
